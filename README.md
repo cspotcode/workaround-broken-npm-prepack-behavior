c7b7dd5e00f90d548110a6cd837120778439dafa
Andrew Bradley <cspotcode@gmail.com>
2023-05-06 20:27:31 -0400

    second commit

--- README.md
+++ README.md
@@ -3,6 +3,18 @@ the source code into an npm module.  git clone -> prepack to build it -> pack in
 
 yarn and pnpm do this.  npm has a bug and does not.
 
+As a workaround, you can add this as a dependency and then declare in package.json:
+
+```json
+  "scripts": {
+    "prepack": "your prepack script here",
+    "prepare": "workaround-broken-npm-prepack-behavior prepack"
+  }
+```
+
+It will run prepack only when prepare is being invoked by npm to set up a git dependency, not for any
+other reason, such as cloning your project and running `npm install`.
+
 ### How it works
 
 We detect when env var npm_config_local_prefix is a subdirectory of npm_config_cache, because that'll detect the only time in git dep installation where I should run compilation.
--- bin.mjs
+++ bin.mjs
@@ -1,32 +1,48 @@
+#!/usr/bin/env node
 import {spawnSync} from 'child_process';
-import {normalize, relative} from 'path';
+import {isAbsolute, normalize, relative} from 'path';
+
+const relevant = {};
+for(const key of Object.keys(process.env)) {
+    if(key.startsWith('npm')) relevant[key] = process.env[key];
+}
+console.dir(relevant);
 const scriptName = process.argv[2];
 const {
     npm_config_local_prefix,
-    npm_config_cache
+    npm_config_cache,
+    npm_package_resolved,
+    npm_package_json,
+    npm_node_execpath,
+    npm_execpath
 } = process.env;
 
 function main() {
     if(isInstallingAsGitDepInNpm()) {
-        console.log('Running `npm prepare`');
+        console.log(`Detected installation as git dependency; running \`npm ${scriptName}\``);
         npmRun(scriptName);
     } else {
-        console.log('Not running `npm prepare`');
+        console.log(`Not a git dependency installation; skipping \`npm ${scriptName}\``);
     }
 }
 
 function isInstallingAsGitDepInNpm() {
-    if(!npm_config_cache || !npm_config_local_prefix) return false;
-    const normalizedNpmConfigLocalPrefix = normalize(npm_config_local_prefix);
+    if(!npm_config_cache) return false;
     const normalizedNpmConfigCache = normalize(npm_config_cache);
-    const rel = relative(normalizedNpmConfigCache, normalizedNpmConfigLocalPrefix);
-    if(/^\.[\/\\]/.test(rel)) {
-        return true;
+
+    // Check if any of these paths are within npm's cache directory
+    for(const path of [npm_package_json, npm_package_resolved, npm_config_local_prefix]) {
+        if (!path) continue;
+        // If local prefix is subdirectory of cache, assume we're being installed as
+        // a git dep
+        const normalized = normalize(path);
+        const rel = relative(normalizedNpmConfigCache, normalized);
+        if(!isAbsolute(rel) && !rel.startsWith('..')) return true;
     }
 }
 
 function npmRun(scriptName) {
-    spawnSync(npm_bin, ['run', scriptName], {
+    spawnSync(npm_node_execpath, [npm_execpath, 'run', scriptName], {
         stdio: 'inherit',
     });
 }
